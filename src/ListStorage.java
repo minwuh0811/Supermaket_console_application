ad436cac38a4392727b7a0f6d19abf247b0197bc
minwuh0811 <your_email@youremail.com>
2019-02-10 04:32:10 +0100

    second commit

--- src/ListStorage.java
+++ src/ListStorage.java
@@ -183,8 +183,10 @@ public class ListStorage implements Storage {
         int length=orders.size();
         String text="";
         for (int i=0; i<length; i++) {
-            text+="CustomerID: " + orders.get(i).getCustomer().getCustomerID() + "  Customer Name: " + orders.get(i).getCustomer().getName()+ '\n'
-                    + "  Product Name: "+orders.get(i).getProducthem().getProduct().getProductname()+ "  Amount: "+ orders.get(i).getProducthem().getAmount()
+            text+="CustomerID: " + orders.get(i).getCustomer().getCustomerID()
+                    + "  Customer Name: " + orders.get(i).getCustomer().getName()+ '\n'
+                    + "  Product Name: "+orders.get(i).getProducthem().getProduct().getProductname()
+                    + "  Amount: "+ orders.get(i).getProducthem().getAmount()
                     +"  Product Price: "+ orders.get(i).getProducthem().getProduct().getPrices()+ '\n';
         }
         WriteTextFile(file,text);
@@ -210,10 +212,12 @@ public class ListStorage implements Storage {
         java.io.File file_OrderSearch = new java.io.File(path);
         int length=ordersearches.size();
         double sum=0;
-        String text="CustomerID: " + ordersearches.get(0).getCustomer().getCustomerID() + "  Customer Name: " + ordersearches.get(0).getCustomer().getName() + '\n';
+        String text="CustomerID: " + ordersearches.get(0).getCustomer().getCustomerID()
+                + "  Customer Name: " + ordersearches.get(0).getCustomer().getName() + '\n';
         for (int i=0; i<length; i++) {
             sum+=ordersearches.get(i).getProducthem().getAmount()*ordersearches.get(i).getProducthem().getProduct().getPrices();
-            text+="Product Name: "+ordersearches.get(i).getProducthem().getProduct().getProductname()+ "  Amount: "+ ordersearches.get(i).getProducthem().getAmount()
+            text+="Product Name: "+ordersearches.get(i).getProducthem().getProduct().getProductname()
+                    + "  Amount: "+ ordersearches.get(i).getProducthem().getAmount()
                     +"  Product Price: "+ ordersearches.get(i).getProducthem().getProduct().getPrices()+ '\n';
         }
         text+= "Total Payment: " + sum +'\n';
@@ -227,7 +231,9 @@ public class ListStorage implements Storage {
     public void PrintCustomer() {
         int length=customers.size();
         for (int i=0; i< length; i++) {
-            System.out.println("CustomerID: "+ customers.get(i).getCustomerID()+ "  Customer Name: " +customers.get(i).getName()+ "  Customer City "+customers.get(i).getCity());
+            System.out.println("CustomerID: "+ customers.get(i).getCustomerID()
+                    + "  Customer Name: " +customers.get(i).getName()
+                    + "  Customer City "+customers.get(i).getCity());
         }
     }
     public Customer findCustomer(int customerID){
@@ -246,7 +252,9 @@ public class ListStorage implements Storage {
     public void PrintProduct() {
         int length=products.size();
         for (int i=0; i< length; i++) {
-            System.out.println("No."+ i+ " Product Name: "+ products.get(i).getProductname() + " Product Price: " +products.get(i).getPrices());
+            System.out.println("No."+ i
+                    + " Product Name: " + products.get(i).getProductname()
+                    + " Product Price: " +products.get(i).getPrices());
         }
     }
     public Product findProduct(int input) {
